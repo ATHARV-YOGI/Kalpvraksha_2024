4b2c4df3a27985a85c5bad4ffe25ce65d2c1a4cb
Atharv Yogi <atharvyogi123@gmail.com>
2025-03-25 21:19:23 +0530

    commit

--- Day 18 Sorting Algo's/selection_sort.c
+++ Day 18 Sorting Algo's/selection_sort.c
@@ -2,9 +2,9 @@
 
 
 void swap(int *a, int *b){
-    *a=*a^*b;
-    *b=*a^*b;
-    *a=*a^*b;
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
 
 void show(int *arr, int n){
@@ -14,7 +14,7 @@ void show(int *arr, int n){
 }
 
 void selection_sort(int *arr, int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         int min=i;
         for(int j=i;j<n;j++){
           if(arr[j]<arr[min]){
