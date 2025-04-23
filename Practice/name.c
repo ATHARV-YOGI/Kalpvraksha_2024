f56d59fc201d96f35eba2ad6caaa4ddcd57117f6
Atharv Yogi <atharvyogi123@gmail.com>
2025-04-23 22:44:53 +0530

    commit

--- Practice/name.c
+++ Practice/name.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void string_reverse(char *name){
-    
-    int n = sizeof(name)/sizeof(name[0]);
-    printf("%d",n);
-    for(int i = n+6; i >= 0 ; i--){
-        printf("%c",name[i]);
-        
+void string_reverse(char *name)
+{
 
+    int n = sizeof(name) / sizeof(name[0]);
+    printf("%d", n);
+    for (int i = n + 6; i >= 0; i--)
+    {
+        printf("%c", name[i]);
     }
 }
 
-void main(){
-    char name[]= "Atharv Yogi";
-    int n = sizeof(name)/sizeof(name[0]);
-    printf("%d",n);
+void main()
+{
+    char name[] = "Atharv Yogi";
+    int n = sizeof(name) / sizeof(name[0]);
+    printf("%d", n);
     string_reverse(name);
-    
 }
